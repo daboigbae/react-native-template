317e923063895765a130040d690b38ef7c131b16
Gabriel Higareda <ghigared@gmail.com>
2022-09-23 06:40:49 -0600

    Added component lib to project

--- template/src/screens/HomeScreen.js
+++ template/src/screens/HomeScreen.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 
+import {Button} from "@digital-art-dealers/react-native-component-lib";
 const HomeScreen = () => {
 	return (
-		<View className="h-full w-full justify-center items-center">
-			<Text>Home Screen</Text>
+		<View className="h-full w-full justify-center items-center px-4">
+			<Button.Primary
+				backgroundColor="bg-blue-600"
+				onPress={() => {}}
+				label="Change this button"
+			/>
 		</View>
 	);
 };
