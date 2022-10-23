92e20abdd7e74cdce4111803c6d0e5856a8d2346
Manbeartrain <ahigareda15@gmail.com>
2022-10-22 22:11:23 -0600

    Updated component lib and button

--- template/src/screens/HomeScreen.js
+++ template/src/screens/HomeScreen.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { View } from "react-native";
 
-import {Button} from "@digital-art-dealers/react-native-component-lib";
+import { Button } from "@digital-art-dealers/react-native-component-lib";
 const HomeScreen = () => {
 	return (
-		<View className="h-full w-full justify-center items-center px-4">
-			<Button.Primary
-				backgroundColor="bg-blue-600"
+		<View className="h-full w-full justify-center px-4">
+			<Button
+				buttonColor="bg-blue-600"
+				textColor="text-white"
 				onPress={() => {}}
 				label="Change this button"
 			/>
