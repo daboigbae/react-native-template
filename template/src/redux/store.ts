bd9dec983a3b646751dadd1d2be7e1293a762c3e
Manbeartrain <ghigared@gmail.com>
2024-03-06 12:27:42 -0700

    Updated dependencies

--- template/src/redux/store.ts
+++ template/src/redux/store.ts
@@ -1,26 +1,26 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import {configureStore} from "@reduxjs/toolkit";
-import {combineReducers} from "redux";
-import {persistReducer} from "redux-persist";
-import thunk from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import {thunk} from "redux-thunk";
 
 import UserSlice from "./UserSlice";
 
 const reducers = combineReducers({
-	UserSlice,
+	UserSlice
 });
 
 const persistConfig = {
 	key: "root",
 	storage: AsyncStorage,
-	whitelist: [], // add reducers you want to persist here
+	whitelist: [] // add reducers you want to persist here
 };
 
 const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
 	reducer: persistedReducer,
-	middleware: [thunk],
+	middleware: [thunk]
 });
 
 export default store;
