efac4eef06b758093dd5491651b01026671096e6
Manbeartrain <ghigared@gmail.com>
2024-03-06 13:56:08 -0700

    Added Api Object
    Added custom example hook to execute test api call

--- template/src/screens/HomeScreen.tsx
+++ template/src/screens/HomeScreen.tsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import {useLayoutEffect} from "react";
-import {Text, View} from "react-native";
+import {ActivityIndicator, Text, TouchableOpacity, View} from "react-native";
 
 import {useNavigation} from "@react-navigation/native";
 import moment from "moment";
 
+import useExample from "@hooks/useExample";
 import useLocalize from "@hooks/useLocalize";
+
+// For example purposes. Replace with your own data structure or remove if not needed
+interface DataProps {
+	attributes: {
+		body: string;
+	};
+}
 
 const HomeScreen = () => {
 	const navigation = useNavigation();
 	const {translate} = useLocalize();
+
+	const {get, isLoading} = useExample();
+
+	const [data, setData] = useState<DataProps | null>(null);
+
+	const getExampleData = async () => {
+		const results = await get();
+		setData(results[0]);
+	};
 
 	// TODO: This can be removed or changed according to project needs
 	useLayoutEffect(() => {
@@ -18,15 +35,30 @@ const HomeScreen = () => {
 		});
 	}, [navigation, translate]);
 
-
 	return (
 		<View className="h-full w-full justify-center px-4">
 			<Text className="text-2xl font-bold text-center mt-8">
-                Todays Date
+				Todays Date
 			</Text>
 			<Text className="text-center text-lg">
 				{moment(new Date()).format("YYYY-MM-DD")}
 			</Text>
+			<TouchableOpacity
+				onPress={getExampleData}
+				className="w-full h-12 justify-center items-center bg-blue-600 rounded-lg mt-4">
+				{isLoading ? (
+					<ActivityIndicator color="white" />
+				) : (
+					<Text className="text-white text-center">
+						Get Example Data
+					</Text>
+				)}
+			</TouchableOpacity>
+			{data && (
+				<Text className="text-center mt-4">
+					{`Example Data: ${data.attributes.body}`}
+				</Text>
+			)}
 		</View>
 	);
 };
--- template/src/screens/LandingScreen.tsx
+++ template/src/screens/LandingScreen.tsx
@@ -1,18 +1,17 @@
-import React, { useEffect } from "react";
-import { View } from "react-native";
+import React, {useEffect} from "react";
+import {View} from "react-native";
 
-import { ParamListBase } from "@react-navigation/native";
-import { StackScreenProps } from "@react-navigation/stack";
+import {ParamListBase} from "@react-navigation/native";
+import {StackScreenProps} from "@react-navigation/stack";
 import LottieView from "lottie-react-native";
 
-import { NAVIGATOR_BOTTOM_TAB } from "@utils/screens";
+import {NAVIGATOR_BOTTOM_TAB} from "@utils/screens";
 
 import LandingAnimation from "../assets/lottie/landingAnimation.json";
 
+type Props = StackScreenProps<ParamListBase, "Landing Stack">;
 
-type Props = StackScreenProps<ParamListBase, "Landing Stack"	>;
-
-const LandingScreen: React.FC<Props> = ({ navigation }) => {
+const LandingScreen: React.FC<Props> = ({navigation}) => {
 	useEffect(() => {
 		setTimeout(() => {
 			navigation.replace(NAVIGATOR_BOTTOM_TAB);
@@ -20,10 +19,10 @@ const LandingScreen: React.FC<Props> = ({ navigation }) => {
 	}, [navigation]);
 
 	return (
-		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+		<View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
 			<LottieView
 				source={LandingAnimation}
-				style={{ width: "70%" }}
+				style={{width: "70%"}}
 				autoPlay
 				loop
 			/>
