e6caa3d3be1c4cf4b0920b675a88fd3b51d580b0
Manbeartrain <ghigared@gmail.com>
2024-03-06 22:11:43 -0700

    Added Avatar component

---
+++ template/src/components/common/Avatar/index.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {Image, Text, View} from "react-native";
+
+import Loader from "../Loader";
+import {AvatarProps} from "./types";
+
+const Avatar: React.FC<AvatarProps> = ({
+	type = "circle",
+	username,
+	isLoading,
+	image,
+	size,
+}) => {
+	const avatarContainerBaseStyle =
+		type === "square"
+			? "rounded-none"
+			: type === "circle"
+				? "rounded-full"
+				: "rounded-lg";
+
+	const getUsernameInitials = (username: string) => {
+		const nameArray = username.split(" ");
+		const firstWord = nameArray[0];
+		const firstInitial = firstWord.charAt(0);
+		const secondInitial =
+			nameArray.length > 1
+				? nameArray[1].charAt(0)
+				: firstWord.length > 1
+					? firstWord.charAt(1)
+					: "";
+		return `${firstInitial}${secondInitial}`;
+	};
+
+	return (
+		<View
+			style={{
+				height: size,
+				width: size,
+			}}
+			className={`${avatarContainerBaseStyle} bg-gray-300 justify-center items-center `}>
+			{isLoading ? (
+				<Loader />
+			) : image ? (
+				<Image
+					resizeMethod="resize"
+					source={{uri: image}}
+					className={`${avatarContainerBaseStyle} w-full h-full`}
+				/>
+			) : (
+				username && <Text>{getUsernameInitials(username)}</Text>
+			)}
+		</View>
+	);
+};
+
+export default Avatar;
--- template/src/screens/HomeScreen.tsx
+++ template/src/screens/HomeScreen.tsx
@@ -5,6 +5,7 @@ import {ScrollView, Text, View} from "react-native";
 import {useNavigation} from "@react-navigation/native";
 import moment from "moment";
 
+import Avatar from "@components/common/Avatar";
 import AwareView from "@components/common/AwareView";
 import Button from "@components/common/Button";
 import TextInput from "@components/common/TextInput";
@@ -45,60 +46,13 @@ const HomeScreen = () => {
 				contentContainerStyle={{flexGrow: 1, paddingBottom: "15%"}}
 				className="w-full bg-white h-full">
 				<View className=" h-full w-full justify-center px-4">
-					<Text className="text-2xl font-bold text-center mt-8">
-						Todays Date
-					</Text>
-					<Text className="text-center text-lg">
-						{moment(new Date()).format("YYYY-MM-DD")}
-					</Text>
-
-					<Button
-						buttonStyle="w-full bg-blue-600 h-12 rounded-lg justify-center items-center"
-						onPress={getExampleData}
-						label="Get Example Data"
-						isLoading={isLoading}
-						isDisabled={isLoading}
-					/>
-					{data && (
-						<Text className="text-center mt-4">
-							{`Example Data: ${data.attributes.body}`}
-						</Text>
-					)}
-					<View className="h-8" />
-					<TextInput
-						type="password"
-						placeholder="Enter your name..."
-						label="Name"
-						onChangeText={setName}
-						value={name}
-					/>
-					<Text className="text-2xl font-bold text-center mt-8">
-						Todays Date
-					</Text>
-					<Text className="text-center text-lg">
-						{moment(new Date()).format("YYYY-MM-DD")}
-					</Text>
-
-					<Button
-						buttonStyle="w-full bg-blue-600 h-12 rounded-lg justify-center items-center"
-						onPress={getExampleData}
-						label="Get Example Data"
-						isLoading={isLoading}
-						isDisabled={isLoading}
-					/>
-					{data && (
-						<Text className="text-center mt-4">
-							{`Example Data: ${data.attributes.body}`}
-						</Text>
-					)}
-					<View className="h-8" />
-					<TextInput
-						type="password"
-						placeholder="Enter your name..."
-						label="Name"
-						onChangeText={setName}
-						value={name}
-					/>
+					<View className="self-center">
+						<Avatar
+							image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAF3jvkhiHrC4W-Vu9J_0A1kvni4C5qZrj1w&usqp=CAU"
+							username="Manbeartrain"
+							size={100}
+						/>
+					</View>
 					<Text className="text-2xl font-bold text-center mt-8">
 						Todays Date
 					</Text>
